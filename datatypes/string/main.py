bc3c64386a76b14fd791c9792e3914dfff7937f5
Deepak <deepak2603dk@gmail.com>
2024-04-18 17:03:25 +0530

    string slicing

---
+++ datatypes/string/main.py
@@ -0,0 +1,16 @@
+string1 = "hello World"
+string2 = "hello 'World'"
+
+print(string1)
+print(string2)
+
+
+#string in multyline
+
+multiLineString = '''
+                this is ,
+                multiline,
+                string.
+                  '''
+
+print(multiLineString)
---
+++ datatypes/string/slicing.py
@@ -0,0 +1,19 @@
+word = "Hello, World!"
+
+
+#It is like an array start from 2 index but end with n-1 index
+print(word[2:5])  #llo
+
+# Slice From the Start
+print(word[:5])   #Hello
+
+# Slice To the End
+print(word[7:])   #World!
+
+
+
+# Negative Indexing
+# here the concept is reversed the string start with n-1 index and end with exact index
+print(word[-5:-2])  #orl
+
+
