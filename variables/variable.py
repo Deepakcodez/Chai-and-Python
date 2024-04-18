6ce751c4a7a2ffa75e4817ec0fab41cb50d8c3c6
Deepak <deepak2603dk@gmail.com>
2024-04-18 16:39:12 +0530

    variables in python

---
+++ variables/variable.py
@@ -0,0 +1,25 @@
+num1 = 22;
+string1 = "hello";
+string2 = 'world';
+
+print(num1, string1, string2);
+
+
+# Assign Multiple Values
+x, y, z = "Xvalue", 9, "Zvalue";
+
+print(x,y,z)
+
+
+#Unpack a Collection
+fruits = ["mango", "banana","grapes"]
+
+x1 , y2 , z3 = fruits
+
+print( x1,"\n",y2,"\n",z3)
+
+
+#multiplying strings
+print("Hello"*3)
+
+
