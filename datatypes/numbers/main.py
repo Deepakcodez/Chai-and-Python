972050701d4ffaa2eaf72c36ba0e4aff2f3310bf
Deepak <deepak2603dk@gmail.com>
2024-04-18 16:47:46 +0530

    numbers in python

---
+++ datatypes/numbers/main.py
@@ -0,0 +1,26 @@
+#Number Data Type
+
+# There are three numeric types in Python:
+
+# int
+# float
+# complex
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x)," ", x)
+print(type(y)," ", y)
+print(type(z)," ", z)
+
+
+
+# type conversion
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(type(a))
+print(type(b))
+print(type(c))
