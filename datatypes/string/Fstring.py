f9c85a11a64d0047ab0d367b2b227b478a24cd0e
Deepak <deepak2603dk@gmail.com>
2024-04-18 17:24:30 +0530

    fstring and modify string

---
+++ datatypes/string/Fstring.py
@@ -0,0 +1,11 @@
+age = 20;
+print(f"my age is {age}")
+
+
+#A modifier is included by adding a colon : followed by a legal formatting type, like .2f which means fixed point number with 2 decimals:
+sallery = 333.3344
+print(f"my sallery is {sallery:.2f}")
+
+
+#A placeholder can contain Python code, like math operations:
+print(f"multiplication of sallery and age is {age * sallery}")
---
+++ datatypes/string/modifyString.py
@@ -0,0 +1,20 @@
+string1 = "   hello, World     "
+
+# to uppercase
+print(string1.upper())
+
+# to lowercase
+print(string1.lower())
+
+
+# Remove Whitespace
+print(string1.strip())
+
+
+#Replace String
+print(string1.replace("World","duniya"))
+
+#Split String
+#The split() method returns a list where the text between the specified separator becomes the list items.
+print(string1.split(","))
+print(string1.strip().split(","))
